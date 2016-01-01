2d493109ab4e0bf98050136d95662d949b20756c
Spencer Watson <spencer@wattydev.com>
2016-01-01 12:47:19 -0800

    test

--- cardsAgainstHumanity/api/newGame.php
+++ cardsAgainstHumanity/api/newGame.php
@@ -14,25 +14,26 @@ $newId = uniqid();
 $data = new SSEData('file',array('path'=>'./data'));
 
 function create() {
-    $newId = uniqid();
-    if (!empty(json_decode($data->get($newId))) {
-        // setcookie('currentGame', $newId, mktime(). time()+60*60*24);
-        // $data->set($newId,json_encode(array(
-        //     'players'=>array(),
-        //     'host'=>$_GET['host'],
-        //     'deck'=>file_get_contents('cards.json'),
-        //     'discarded'=>array(),
-        //     'id'=>$newId,
-        //     'nickname'=>$_GET['nickname'],
-        //     'started'=>false,
-        //     'log'=>array(),
-        //     'updated'=>time()
-        // )))
-        // http_response_code(200);
-        echo $newId;
-        return;
-    } else {
-        create();
+    echo 'test';
+    // $newId = uniqid();
+    // if (!empty(json_decode($data->get($newId))) {
+    //     setcookie('currentGame', $newId, mktime(). time()+60*60*24);
+    //     $data->set($newId,json_encode(array(
+    //         'players'=>array(),
+    //         'host'=>$_GET['host'],
+    //         'deck'=>file_get_contents('cards.json'),
+    //         'discarded'=>array(),
+    //         'id'=>$newId,
+    //         'nickname'=>$_GET['nickname'],
+    //         'started'=>false,
+    //         'log'=>array(),
+    //         'updated'=>time()
+    //     )))
+    //     http_response_code(200);
+    //     echo $newId;
+    //     return;
+    // } else {
+    //     create();
     }
 }
 create();
