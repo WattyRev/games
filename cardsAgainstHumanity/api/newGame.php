a56ce764250f020e81e3cd245a54d54e7af8467a
Spencer Watson <spencer@wattydev.com>
2016-01-01 13:05:55 -0800

    test

--- cardsAgainstHumanity/api/newGame.php
+++ cardsAgainstHumanity/api/newGame.php
@@ -16,19 +16,20 @@ function create() {
     $newId = uniqid();
     // if (!empty(json_decode($data->get($newId))) {
         setcookie('currentGame', $newId, time()+60*60*24);
-        $data->set('test',json_encode(
-            array(
-                'players'=>array(),
-                // 'host'=>$_GET['host'],
-                // 'deck'=>file_get_contents('cards.json'),
-                // 'discarded'=>array(),
-                // 'id'=>$newId,
-                // 'nickname'=>$_GET['nickname'],
-                // 'started'=>false,
-                // 'log'=>array(),
-                'updated'=>time()
-            )
-        ));
+        var_dump($data);
+        // $data->set('test',json_encode(
+        //     array(
+        //         'players'=>array(),
+        //         // 'host'=>$_GET['host'],
+        //         // 'deck'=>file_get_contents('cards.json'),
+        //         // 'discarded'=>array(),
+        //         // 'id'=>$newId,
+        //         // 'nickname'=>$_GET['nickname'],
+        //         // 'started'=>false,
+        //         // 'log'=>array(),
+        //         'updated'=>time()
+        //     )
+        // ));
         // http_response_code(200);
         echo $newId;
         return;
