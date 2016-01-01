3b0faf55b15f46b015f31ca637cd767d7a4d2dd5
Spencer Watson <spencer@wattydev.com>
2015-12-31 16:04:42 -0800

    Update see url

--- cardsAgainstHumanity/app/js/services/sse.js
+++ cardsAgainstHumanity/app/js/services/sse.js
@@ -1,5 +1,5 @@
 app.service('sse', ['$rootScope', function($root) {
-    var data = new EventSource('../api/events.php');
+    var data = new EventSource('http://games.wattydev.com/cardsAgainstHumanity/api/events.php');
 
     data.addEventListener('update',function(e){
         $root.broadcast('update', e);
