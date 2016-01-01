370969c7d9e54766aaf2b74217f42f1c0e03e21e
Spencer Watson <spencer@wattydev.com>
2016-01-01 12:55:06 -0800

    test

--- cardsAgainstHumanity/api/newGame.php
+++ cardsAgainstHumanity/api/newGame.php
@@ -11,7 +11,7 @@ if (!isset($_GET['host']) || !isset($_GET['nickname'])) {
 }
 
 $newId = uniqid();
-$data = new SSEData('file',array('path'=>'data'));
+$data = new SSEData('file',array('path'=>'./data'));
 
 function create() {
     //echo 'test';
