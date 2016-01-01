841d8a7bee8c577d80cdc15ab2035a3b9b106907
Spencer Watson <spencer@wattydev.com>
2016-01-01 12:33:36 -0800

    test

--- cardsAgainstHumanity/api/newGame.php
+++ cardsAgainstHumanity/api/newGame.php
@@ -2,37 +2,37 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Key");
 header("Access-Control-Allow-Methods: PUT");
-
-require_once('src/libsse.php');
-
-if (!isset($_POST['host']) || !isset($_POST['nickname'])) {
-    http_response_code(400);
-    echo 'Host and nickname are required';
-    return;
-}
-
-$newId = uniqid();
-$data = new SSEData('file',array('path'=>'./data'));
-
-function create() {
-    $newId = uniqid();
-    if (!empty(json_decode($data->get($newId))) {
-        setcookie('currentGame', $newId, mktime(). time()+60*60*24);
-        $data->set($newId,json_encode(array(
-            'players'=>array(),
-            'host'=>$_POST['host'],
-            'deck'=>file_get_contents('cards.json'),
-            'discarded'=>array(),
-            'id'=>$newId,
-            'nickname'=>$_POST['nickname'],
-            'started'=>false,
-            'log'=>array(),
-            'updated'=>time()
-        )))
-        http_response_code(200);
-        echo $newId;
-        return;
-    } else {
-        create();
-    }
-}
+echo 'test';
+// require_once('src/libsse.php');
+//
+// if (!isset($_POST['host']) || !isset($_POST['nickname'])) {
+//     http_response_code(400);
+//     echo 'Host and nickname are required';
+//     return;
+// }
+//
+// $newId = uniqid();
+// $data = new SSEData('file',array('path'=>'./data'));
+//
+// function create() {
+//     $newId = uniqid();
+//     if (!empty(json_decode($data->get($newId))) {
+//         setcookie('currentGame', $newId, mktime(). time()+60*60*24);
+//         $data->set($newId,json_encode(array(
+//             'players'=>array(),
+//             'host'=>$_POST['host'],
+//             'deck'=>file_get_contents('cards.json'),
+//             'discarded'=>array(),
+//             'id'=>$newId,
+//             'nickname'=>$_POST['nickname'],
+//             'started'=>false,
+//             'log'=>array(),
+//             'updated'=>time()
+//         )))
+//         http_response_code(200);
+//         echo $newId;
+//         return;
+//     } else {
+//         create();
+//     }
+// }
--- cardsAgainstHumanity/app/js/services/api.js
+++ cardsAgainstHumanity/app/js/services/api.js
@@ -1,6 +1,6 @@
 app.service('api', ['$http', '$rootScope', function($http, $root) {
 
-    var base = 'http://games.wattydev.com/cardsAgainstHumanity';
+    var base = 'http://games.wattydev.com/cardsAgainstHumanity/api';
     function _put(uri, payload, params) {
         return $http.put(base + uri + '.php', payload, {params:params});
     }
