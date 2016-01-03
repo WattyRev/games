9c3fabbb23e3e04d0cf2da9c9e1ec720d4686a24
Spencer Watson <spencer@wattydev.com>
2016-01-03 11:30:45 -0800

    setup watch on all games

--- cardsAgainstHumanity/app/js/services/api.js
+++ cardsAgainstHumanity/app/js/services/api.js
@@ -4,13 +4,16 @@ app.service('api', ['$http', '$rootScope', function($http, $root) {
     function _put(uri, payload, params) {
         return $http.put(base + uri + '.php', payload, {params:params});
     }
+    function _get(uri, params) {
+        return $http.get(base + uri + '.php', {params:params});
+    }
 
     return {
         update: function() {
             return _put('/update', $session.model, {});
         },
         createGame: function() {
-            return _put('/newGame', null,
+            return _get('/newGame',
                 {
                     host: $root.userId,
                     nickname: util.makeId()
